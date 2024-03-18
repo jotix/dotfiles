dbd16bb23d60062b7625893d36023f4c8edd9bf7
jotix <jujodeve@gmail.com>
2024-03-18 11:08:20 -0300

    qmk

--- .scripts/ubuntu-wsl.sh
+++ .scripts/ubuntu-wsl.sh
@@ -2,10 +2,13 @@
 #
 sudo apt install git gcc
 
-sudo snap install go nvim --classic
+sudo snap install go nvim --classic pipx
 
 ### powerline-go
 go install github.com/justjanne/powerline-go@latest
 
 ### lazygit
 go install github.com/jesseduffield/lazygit@latest
+
+### qmk
+pipx install qmk
